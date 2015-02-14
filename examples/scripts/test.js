e509c1d2ab58643130b322db973c1e5b6c0e1ad3
Alex Bigelow <alex.r.bigelow@gmail.com>
2015-02-13 18:35:23 -0700

    Okay, NOW arcs are working?

--- examples/scripts/test.js
+++ examples/scripts/test.js
@@ -1,15 +1,40 @@
-var test = d3.selectAll('#Layer_1').selectAll('path').data([0]);
-var testEnter = test.enter();
-testEnter.append('path');
+var data = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+];
 
-test.attr('d','M100,350 l 50,-25 ' +
-           'a25,25 -30 0,1 50,-25 l 50,-25 ' +
-           'a25,50 -30 0,1 50,-25 l 50,-25 ' +
-           'a25,75 -30 0,1 50,-25 l 50,-25 ' +
-           'a25,100 -30 0,1 50,-25 l 50,-25')
-    .attr('stroke','black')
-    .attr('fill','none')
-    .attr('stroke-width','0.5px');
+var arcSections = d3.selectAll('#Layer_1').selectAll('g').data(data);
+var arcSectionsEnter = arcSections.enter().append('g');
+
+arcSectionsEnter.attr('transform', function (d) {
+    return 'translate(' + d[0] * 300 + ',' + d[1] * 200 + ')';
+});
+
+arcSectionsEnter.append('path')
+    .attr('class', 'background')
+    .attr('d', function (d) {
+        var result = '',
+            i;
+        for (i = 0; i < data.length; i += 1) {
+            result += 'M 125,75 a100,50 0 ';
+            result += data[i].join(',');
+            result += ' 100,50';
+        }
+        return result;
+    })
+    .style('stroke', 'black')
+    .style('fill', 'none')
+    .style('stroke-width', '1');
+arcSectionsEnter.append('path')
+    .attr('class', 'foreground')
+    .attr('d', function (d) {
+        return 'M 125,75 a100,50 0 ' + d.join(',') + ' 100,50';
+    })
+    .style('stroke', 'red')
+    .style('fill', 'none')
+    .style('stroke-width', '6');
 
 /*var test = d3.selectAll('#Layer_1').selectAll('text').data([0]);
 var testEnter = test.enter();
